d9503eda4337a00d6d10a7a5f33a15947b0174a1
racheldmiller <rachelmiller415@gmail.com>
2019-06-24 07:35:52 -0700

    progress with files

--- routes/handlers.js
+++ routes/handlers.js
@@ -18,4 +18,20 @@ router.get("/", function(req, res) {
   });
 });
 
+router.post("/add", (req, res) => {
+  const burgerName = req.body.burger_name;
+  orm.insertOne(burgerName, function(err, burger) {
+    if (err) {
+      return res.status(401).json({
+        message: "Not able to add this burger"
+      });
+    }
+    return res.json({
+      burger_name: burgerName,
+      id: burger.insertId,
+      is_favorite: 0
+    });
+  });
+});
+
 module.exports = router;
