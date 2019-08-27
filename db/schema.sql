0f52b4e1eab212a4d19b67caebd60884e0983dd3
racheldmiller <rachelmiller415@gmail.com>
2019-08-26 22:33:14 -0700

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,14 @@
+-- CREATE AND USE DB
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
 
+-- Remove table if it exists before starting again
+DROP TABLE IF EXISTS burgers;
+
+-- BURGERS TABLE 
 CREATE TABLE burgers(
 	id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(255) NOT NULL,
-    is_favorite BOOLEAN,
-
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
